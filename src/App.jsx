45d969032af6608f96fcd05574d29c2b64552008
Tarasdeveloper <tomahavki1@gmail.com>
2023-12-01 14:27:27 +0200

    created

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from 'components/navbar/Navbar';
+import Footer from 'components/Footer/Footer';
+import Home from 'pages/Home';
+import Projects from 'pages/Projects';
+import ProjectPage from 'pages/ProjectPage';
+import Contacts from 'pages/Contacts';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from 'utils/scrollToTop';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <ScrollToTop />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/product/:id" element={<ProjectPage />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+};
+
+export default App;
